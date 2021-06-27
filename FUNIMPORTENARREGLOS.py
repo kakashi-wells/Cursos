77200af9da684b21e20d92d6c142616a3175dfb3
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-26 23:39:12 -0500

    actualizando FUNIMPORTENARREGLOS.py

--- FUNIMPORTENARREGLOS.py
+++ FUNIMPORTENARREGLOS.py
@@ -2,9 +2,11 @@ colors = ["Verde", "Morado", "Negro", "Verde"]
 numbers = {1, 2, 3, 4, 5}
 person = {"name":"kakashi", "lastname": "wells", "age":24}
 animal = {"name":"hawk","colors":"white","age":4}
+print(len(colors))
+print(len(person))
 for value in colors:
     print(value)
+    
 
-person.update(animal)
 for key,value in person.items():
     print(key,"",value)
