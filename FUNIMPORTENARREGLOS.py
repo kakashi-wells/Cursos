3ba39da4755e3f3e2669ee20c4715eba39f58d14
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-26 22:39:54 -0500

    Actualizacion a Funciones importantes en arreglos.

---
+++ FUNIMPORTENARREGLOS.py
@@ -0,0 +1,9 @@
+colors = ["Verde", "Morado", "Negro"]
+person = {"name":"kakashi", "lastname": "wells", "age":24}
+
+print(colors.pop(2))
+for value in colors:
+    print(value)
+
+for key,value in person.items():
+    print(key,"",value)
