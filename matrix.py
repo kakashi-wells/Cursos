0b2e0830eff8bbe46a6a981b48e36ed979cd584b
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-27 03:26:47 -0500

    ssuma de Matrices, resultado final en matrix.py

--- matrix.py
+++ matrix.py
@@ -2,5 +2,9 @@ matrix1 = [[1,2,3],[7,8,9],[13,14,15]]
 matrix2 = [[4,5,6],[10,11,12],[16,17,18]]
 
 matrix3 = [[1,2,3],[7,8,9],[13,14,15]]
-matrix3[0][0]=matrix1[0][0]+matrix2[0][0]
-print(matrix3[0][0])
+
+
+for rowPosition, value in enumerate(matrix1):
+    for coluPosition, value2 in enumerate(value):
+        matrix3[rowPosition][coluPosition]=value2+matrix2[rowPosition][coluPosition]
+print(matrix3)
