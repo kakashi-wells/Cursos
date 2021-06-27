80e4b8d403d92b931db61aec4032898b2355caf9
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-26 23:14:40 -0500

    agregando extension a numeros

--- FUNIMPORTENARREGLOS.py
+++ FUNIMPORTENARREGLOS.py
@@ -1,8 +1,8 @@
 colors = ["Verde", "Morado", "Negro", "Verde"]
 numbers = {1, 2, 3, 4, 5}
 person = {"name":"kakashi", "lastname": "wells", "age":24}
-colors[0]="Azul"
-for value in colors:
+numbers.extend()
+for value in numbers:
     print(value)
 
 for key,value in person.items():
