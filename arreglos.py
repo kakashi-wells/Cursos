8767c892f2583c163902214291cd055d2912655f
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-26 22:24:17 -0500

    MODIFICACION DE VARIANTE EN ¿COMO USAR ARREGLOS?

--- arreglos.py
+++ arreglos.py
@@ -1,8 +1,4 @@
 colors = ["Verde", "Morado", "Negro"]
 person = {"name":"kakashi", "lastname": "wells", "age":24}
 
-for value in colors:
-    print(value)
-
-for value in person:
-    print(value)
+print(person.get("lastname"))
