43bcfe1f4533c0c0842e3df28473a2f7eda1d130
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-27 02:31:47 -0500

    multidimensiona.py

--- multidimensionales.py
+++ multidimensionales.py
@@ -1,6 +1,6 @@
-shoes =[
-{"Nike":"tenis 1", "Converse":"tenis 2", "Vans":"tenis 3"},
-{"Nike":"tenis 4", "Converse":"tenis 5", "Vans":"tenis 6"},
-{"Nike":"tenis 7", "Converse":"tenis 8", "Vans":"tenis 9"}]
+shoes = {
+"Nike":["tenis 1","tenis 2","tenis 3"],
+"Converse":["tenis 3","tenis 4","tenis 5"],
+"Vans":["tenis 6", "tenis 7", "tenis 8"]}
 
-print(shoes[2]["Vans"])
+print(shoes["Converse"][1])
