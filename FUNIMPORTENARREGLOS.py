f377897346af14957c1b5258578476382d810cc3
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-26 23:17:28 -0500

    agregando person.pop(lastname))

--- FUNIMPORTENARREGLOS.py
+++ FUNIMPORTENARREGLOS.py
@@ -1,9 +1,10 @@
 colors = ["Verde", "Morado", "Negro", "Verde"]
 numbers = {1, 2, 3, 4, 5}
 person = {"name":"kakashi", "lastname": "wells", "age":24}
-numbers.extend()
-for value in numbers:
+
+for value in colors:
     print(value)
 
+print(person.pop("lastname"))
 for key,value in person.items():
     print(key,"",value)
