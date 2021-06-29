2a7aa7654825ebcaa668befdefc1773d8bba9205
kakashi-hatake <arthurbedoya99@gmail.com>
2021-06-28 23:55:16 -0500

    Prueba y depuracion en codificacion.py

--- codificacion.py
+++ codificacion.py
@@ -1,7 +1,16 @@
-largo=float(input("Coloca el largo en metros:"))
-ancho = float(input("Coloca el ancho en metros:"))
-m2xcaja = float(input("Coloca los metros cuadrados por caja:"))
-precioxm2 = float(input("Coloca el precio por metro cuadrado:"))
+def validar(message):
+    while True:
+        try:
+            data=float(input("Coloca" + message))
+            return data        
+        except ValueError:
+            print("El dato debe ser entero o decimal")
+
+
+largo=validar("Coloca el largo en metros:")
+ancho = validar("Coloca el ancho en metros:")
+m2xcaja = validar("Coloca los metros cuadrados por caja:")
+precioxm2 = validar("Coloca el precio por metro cuadrado:")
 precioxcaja = (m2xcaja * precioxm2)
 m2Cuarto = largo * ancho
 cajas = m2Cuarto/m2xcaja
